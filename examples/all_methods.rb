848a789c3dfe554558d41d6e84f1f9328f1f3a8c
Wally Glutton <stungeye@gmail.com>
2013-03-23 10:35:41 -0500

    Removed a rate limit testing loop from the all methods example.

--- examples/all_methods.rb
+++ examples/all_methods.rb
@@ -4,10 +4,8 @@ pinboard = GluttonPinboard.new 'username:YOUR_AUTH_TOKEN_HERE'
 
 require 'pp'
 
-  all_my_tags = pinboard.tags_get
-  puts "I have used #{all_my_tags.size} tags."
+all_my_tags = pinboard.tags_get
+puts "I have used #{all_my_tags.size} tags."
 
-2.times do
-  all_my_posts = pinboard.posts_all
-  puts "I have saved #{all_my_posts.size} links."
-end
+all_my_posts = pinboard.posts_all
+puts "I have saved #{all_my_posts.size} links."
